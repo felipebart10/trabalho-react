7eaee4295eef736aaa77c2071be9b8955b3abe43
fcintra4 <github4@faustocintra.com.br>
2021-04-28 14:00:49 +0000

    (28/04) Início da construção da interface do projeto

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -1,27 +1,56 @@
+/*
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
+*/
+
+/*
+  <div className="App">
+    <header className="App-header">
+      <h1>Projeto Karangos</h1>
+      <Button variant="contained" color="primary">Clique aqui!</Button>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+*/
+
+
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow';
+import red from '@material-ui/core/colors/red';
+import TopBar from './ui/TopBar'
+import FooterBar from './ui/FooterBar'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: yellow[500],
+    },
+    secondary: {
+      main: red[500],
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Projeto Karangos</h1>
-        <Button>Clique aqui!</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <TopBar />
+      <FooterBar />
+    </ThemeProvider>
   );
 }
 
