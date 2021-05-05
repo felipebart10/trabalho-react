cb00de239e3c30badf970ab449c576e531987084
fcintra4 <github4@faustocintra.com.br>
2021-05-05 14:03:17 +0000

    (05/05) Término do layout básico e introdução do router

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -24,9 +24,6 @@ import Button from '@material-ui/core/Button'
     </header>
   </div>
 */
-
-
-
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import yellow from '@material-ui/core/colors/yellow';
 import red from '@material-ui/core/colors/red';
@@ -34,6 +31,9 @@ import TopBar from './ui/TopBar'
 import FooterBar from './ui/FooterBar'
 import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/core/styles'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import KarangosList from './routed/KarangosList'
+import KarangosForm from './routed/KarangosForm'
 
 const theme = createMuiTheme({
   palette: {
@@ -51,6 +51,11 @@ const useStyles = makeStyles((theme) => ({
   main: {
     backgroundColor: theme.palette.background.default,
     minHeight: '100vh' // 100% da altura da área de visualização
+  },
+  routed: {
+    padding: '25px',
+    color: theme.palette.text.primary,
+    fontFamily: theme.typography.fontFamily
   }
 }))
 
@@ -58,8 +63,23 @@ function Main() {
   const classes = useStyles()
   return (
     <Box className={classes.main}>
-      <TopBar />
-      <FooterBar />
+      <BrowserRouter> {/*  Inicia a região onde pode haver troca dinâmica de elementos */}
+        <TopBar />
+        <Box id="routed" className={classes.routed}>
+          <Switch> {/* Determina qual elemento será exibido, de acordo com a rota */}
+            
+            <Route path="/list">
+              <KarangosList />
+            </Route>
+
+            <Route path="/new">
+              <KarangosForm />
+            </Route>
+
+          </Switch>
+        </Box>
+        <FooterBar />
+      </BrowserRouter>
     </Box>
   )
 }
