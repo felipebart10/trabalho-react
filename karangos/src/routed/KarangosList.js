fe1b06569bedb2e504e263e45c9a39604682f4ba
fcintra4 <github4@faustocintra.com.br>
2021-05-17 14:13:47 +0000

    (17/05) Implementação da lógica de exclusão

--- karangos/src/routed/KarangosList.js
+++ karangos/src/routed/KarangosList.js
@@ -17,6 +17,9 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
 import AddBoxIcon from '@material-ui/icons/AddBox';
 import { useHistory } from 'react-router-dom'
+import ConfirmDialog from '../ui/ConfirmDialog'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles(theme => ({
   table: {
@@ -45,6 +48,12 @@ export default function KarangosList() {
 
   // Variáveis que conterão dados PRECISAM ser inicializadas como vetores vazios
   const [karangos, setKarangos] = useState([])
+  const [deletable, setDeletable] = useState()        // Código do registro a ser excluído
+  const [dialogOpen, setDialogOpen] = useState(false) // O diálogo de confirmação está aberto?
+  const [sbOpen, setSbOpen] = useState(false)
+  const [sbSeverity, setSbSeverity] = useState('success')
+  const [sbMessage, setSbMessage] = useState('Exclusão realizada com sucesso.')
+  
   const history = useHistory()
 
   useEffect(() => {
@@ -61,8 +70,47 @@ export default function KarangosList() {
   }, []) // Quando a lista de dependências é um vetor vazio, o useEffect()
          // é executado apenas uma vez, no carregamento inicial do componente
 
+  async function deleteItem() {
+    try {
+      await axios.delete(`https://api.faustocintra.com.br/karangos/${deletable}`)
+      setSbSeverity('success')
+      setSbMessage('Exclusão efetuada com sucesso.')
+    }
+    catch(error) {
+      setSbSeverity('error')
+      setSbMessage('ERRO: ' + error.message)
+    }
+    setSbOpen(true)   // Exibe a snackbar
+  }
+
+  function handleDialogClose(result) {
+    setDialogOpen(false)
+
+    // Se o usuário concordou com a exclusão 
+    if(result) deleteItem()
+  }
+
+  function handleDelete(id) {
+    setDeletable(id)
+    setDialogOpen(true)
+  }
+
+  function handleSbClose() {
+    setSbOpen(false)    // Fecha a snackbar
+  }
+
   return (
     <>
+      <ConfirmDialog isOpen={dialogOpen} onClose={handleDialogClose}>
+        Deseja realmente excluir este karango?
+      </ConfirmDialog>
+      
+      <Snackbar open={sbOpen} autoHideDuration={6000} onClose={handleSbClose}>
+        <MuiAlert elevation={6} variant="filled" onClose={handleSbClose} severity={sbSeverity}>
+          {sbMessage}
+        </MuiAlert>
+      </Snackbar>
+      
       <h1>Listagem de Karangos</h1>
       <Toolbar className={classes.toolbar}>
         <Button color="secondary" variant="contained" size="large" 
@@ -107,7 +155,7 @@ export default function KarangosList() {
                 </IconButton>                
               </TableCell>
               <TableCell align="center">
-                <IconButton aria-label="excluir">
+                <IconButton aria-label="excluir" onClick={() => handleDelete(karango.id)}>
                   <DeleteIcon color="error" />
                 </IconButton>
               </TableCell>
